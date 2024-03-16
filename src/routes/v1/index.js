8edf745070d4c9478c89a4e428801cc2991481eb
grsendha <grsendha11@gmail.com>
2024-03-16 16:31:07 +0530

    added email and passowrd validation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const UserController = require('../../controllers/user-controller');
+const { AuthRequestValidator } = require('../../middlewares/index');
 
-router.post('/signup', UserController.create);
-router.post('/signin', UserController.signIn);
-// router.delete('/signup', UserController.destroy);
+router.post('/signup', AuthRequestValidator.validateUserAuth, UserController.create);
+router.post('/signin', AuthRequestValidator.validateUserAuth, UserController.signIn);
+
 
 module.exports = router;
